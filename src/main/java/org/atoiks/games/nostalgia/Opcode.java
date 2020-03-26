b7fdb2a9b3559eccffcc56a7cbda80dd30c62e3a
Paul Teng <plankp@outlook.com>
2020-03-26 08:01:54 -0400

    Perfectly balanced...
    As all things should be.

--- src/main/java/org/atoiks/games/nostalgia/Opcode.java
+++ src/main/java/org/atoiks/games/nostalgia/Opcode.java
@@ -33,45 +33,50 @@ public final class Opcode {
     // (and I guess if you decide to kill-off confusing ones...)
 
     public static final int OP0_MOV_I   = 0 & MASK_OP0;
-    public static final int OP0_ADD_R   = 1 & MASK_OP0;
-    public static final int OP0_SUB_R   = 2 & MASK_OP0;
-    public static final int OP0_AND_R   = 3 & MASK_OP0;
-    public static final int OP0_OR_R    = 4 & MASK_OP0;
-    public static final int OP0_XOR_R   = 5 & MASK_OP0;
-    public static final int OP0_NAND_R  = 6 & MASK_OP0;
-    public static final int OP0_NOR_R   = 7 & MASK_OP0;
-    public static final int OP0_NXOR_R  = 8 & MASK_OP0;
-    public static final int OP0_ADD_I   = 9 & MASK_OP0;
-    public static final int OP0_SUB_I   = 10 & MASK_OP0;
-    public static final int OP0_RSUB_I  = 11 & MASK_OP0;
-    public static final int OP0_JABS_Z  = 12 & MASK_OP0;
-    public static final int OP0_JABS_NZ = 13 & MASK_OP0;
-    public static final int OP0_JABS_GE = 14 & MASK_OP0;
-    public static final int OP0_JABS_GT = 15 & MASK_OP0;
-    public static final int OP0_JABS_LE = 16 & MASK_OP0;
-    public static final int OP0_JABS_LT = 17 & MASK_OP0;
-    public static final int OP0_JREL_Z  = 18 & MASK_OP0;
-    public static final int OP0_JREL_NZ = 19 & MASK_OP0;
-    public static final int OP0_JREL_GE = 20 & MASK_OP0;
-    public static final int OP0_JREL_GT = 21 & MASK_OP0;
-    public static final int OP0_JREL_LE = 22 & MASK_OP0;
-    public static final int OP0_JREL_LT = 23 & MASK_OP0;
-    public static final int OP0_PUSH    = 24 & MASK_OP0;
-    public static final int OP0_POP     = 25 & MASK_OP0;
+    public static final int OP0_MOV_LO  = 1 & MASK_OP0;
+    public static final int OP0_MOV_HI  = 2 & MASK_OP0;
 
-    // TODO: RESHUFFLE
-    public static final int OP0_LDM_HB  = 26 & MASK_OP0;
-    public static final int OP0_STM_HB  = 27 & MASK_OP0;
+    public static final int OP0_ADD_R   = 3 & MASK_OP0;
+    public static final int OP0_SUB_R   = 4 & MASK_OP0;
+    public static final int OP0_AND_R   = 5 & MASK_OP0;
+    public static final int OP0_OR_R    = 6 & MASK_OP0;
+    public static final int OP0_XOR_R   = 7 & MASK_OP0;
+    public static final int OP0_NAND_R  = 8 & MASK_OP0;
+    public static final int OP0_NOR_R   = 9 & MASK_OP0;
+    public static final int OP0_NXOR_R  = 10 & MASK_OP0;
+
+    public static final int OP0_ADD_I   = 11 & MASK_OP0;
+    public static final int OP0_SUB_I   = 12 & MASK_OP0;
+    public static final int OP0_RSUB_I  = 13 & MASK_OP0;
+
+    public static final int OP0_JABS_Z  = 14 & MASK_OP0;
+    public static final int OP0_JABS_NZ = 15 & MASK_OP0;
+    public static final int OP0_JABS_GE = 16 & MASK_OP0;
+    public static final int OP0_JABS_GT = 17 & MASK_OP0;
+    public static final int OP0_JABS_LE = 18 & MASK_OP0;
+    public static final int OP0_JABS_LT = 19 & MASK_OP0;
+
+    public static final int OP0_JREL_Z  = 20 & MASK_OP0;
+    public static final int OP0_JREL_NZ = 21 & MASK_OP0;
+    public static final int OP0_JREL_GE = 22 & MASK_OP0;
+    public static final int OP0_JREL_GT = 23 & MASK_OP0;
+    public static final int OP0_JREL_LE = 24 & MASK_OP0;
+    public static final int OP0_JREL_LT = 25 & MASK_OP0;
+
+    public static final int OP0_PUSH    = 26 & MASK_OP0;
+    public static final int OP0_POP     = 27 & MASK_OP0;
 
     public static final int OP0_RET     = 28 & MASK_OP0;
     public static final int OP0_ENTER   = 29 & MASK_OP0;
     public static final int OP0_LEAVE   = 30 & MASK_OP0;
+
     public static final int OP0_LD_D    = 31 & MASK_OP0;
     public static final int OP0_ST_D    = 32 & MASK_OP0;
     public static final int OP0_LD_W    = 33 & MASK_OP0;
     public static final int OP0_ST_W    = 34 & MASK_OP0;
     public static final int OP0_LD_B    = 35 & MASK_OP0;
     public static final int OP0_ST_B    = 36 & MASK_OP0;
+
     public static final int OP0_SHL_R   = 37 & MASK_OP0;
     public static final int OP0_SHR_R   = 38 & MASK_OP0;
     public static final int OP0_SAR_R   = 39 & MASK_OP0;
@@ -93,17 +98,14 @@ public final class Opcode {
     public static final int OP0_CALL_LE = 53 & MASK_OP0;
     public static final int OP0_CALL_LT = 54 & MASK_OP0;
 
-    public static final int OP0_MOV_LO  = 55 & MASK_OP0;
-    public static final int OP0_MOV_HI  = 56 & MASK_OP0;
-
-    public static final int OP0_LDM_D   = 57 & MASK_OP0;
-    public static final int OP0_STM_D   = 58 & MASK_OP0;
-
-    public static final int OP0_LDM_W   = 59 & MASK_OP0;
-    public static final int OP0_STM_W   = 60 & MASK_OP0;
-
-    public static final int OP0_LDM_LB   = 61 & MASK_OP0;
-    public static final int OP0_STM_LB   = 62 & MASK_OP0;
+    public static final int OP0_LDM_D   = 55 & MASK_OP0;
+    public static final int OP0_STM_D   = 56 & MASK_OP0;
+    public static final int OP0_LDM_W   = 57 & MASK_OP0;
+    public static final int OP0_STM_W   = 58 & MASK_OP0;
+    public static final int OP0_LDM_HB  = 59 & MASK_OP0;
+    public static final int OP0_STM_HB  = 60 & MASK_OP0;
+    public static final int OP0_LDM_LB  = 61 & MASK_OP0;
+    public static final int OP0_STM_LB  = 62 & MASK_OP0;
 
     public static final int OP0_RESV    = 63 & MASK_OP0;
 
